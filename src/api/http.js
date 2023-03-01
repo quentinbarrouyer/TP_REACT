fe22b87492cebbdb67a214789fff5497bb97a2ee
quentin <b18001457>
2023-03-01 14:28:18 +0100

    api+components

---
+++ src/api/http.js
@@ -0,0 +1,52 @@
+const SUPABASE_URL = "https://nbiaxhslvvqyrjtugqem.supabase.co/rest/v1/todos";
+const SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5iaWF4aHNsdnZxeXJqdHVncWVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc1MDc5MjQsImV4cCI6MTk5MzA4MzkyNH0.29GSRlgCEAY7kVEZBepiRjJQn5DaKZK1tIROgBm1Ihw";
+
+
+/**
+ * Permet de modifier le statut de la tâche dans l'API
+ * @param {number} id 
+ * @param {boolean} status 
+ * @returns Promise<{id: number, done: boolean, text: string}>
+ */
+ export const toggleTaskInApi = (id, status) => {
+    return fetch(`${SUPABASE_URL}?id=eq.${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            apiKey: SUPABASE_API_KEY,
+            Prefer: "return=representation",
+        },
+        body: JSON.stringify({ done: status }),
+    });
+}
+
+/**
+ * Ajoute une tâche dans l'API
+ * @param {{text: string, done: boolean}} task 
+ * @returns Promise<{id: number, text: string, done: boolean}>
+ */
+export const addTaskToApi = (task) => {
+    return fetch(SUPABASE_URL, {
+        method: "POST",
+        body: JSON.stringify(task),
+        headers: {
+            "Content-Type": "application/json",
+            apiKey: SUPABASE_API_KEY,
+            Prefer: "return=representation",
+        },
+    })
+     .then((response) => response.json())
+     .then(items => items[0]);
+}
+
+/**
+ * Récupère les donnes des tâches à partir de l'API
+ * @returns Promise<Array<{id: number, text: string, done: boolean}>>
+ */
+export const loadTasksFromApi = () => {
+    return fetch(`${SUPABASE_URL}?order=created_at`, {
+        headers: {
+            apiKey: SUPABASE_API_KEY,
+        },
+    }).then((response) => response.json())
+}
--- src/pages/TodoListPage.js
+++ src/pages/TodoListPage.js
@@ -1,46 +1,63 @@
-import React, { useState } from "react";
+
+import React, { useEffect, useState } from "react";
+import { addTaskToApi, loadTasksFromApi, toggleTaskInApi } from "../api/http";
 import TaskForm from "../components/TaskForm";
 import TodoList from "../components/TodoList";
 
-// On créé ici un tableau TODO_ITEMS qui contient deux objets 
-const TODO_ITEMS = [
-    { id: 1, text: "Faire les courses", done: false },
-    { id: 2, text: "Aller chercher les enfants", done: true },
-];
-
 const TodoListPage = () => {
-    const [state, setState] = useState(TODO_ITEMS);
+    const [state, setState] = useState([]);
 
     const toggle = (id) => {
         // Récupérons l'index de la tâche concernée
         const idx = state.findIndex(task => task.id === id);
-        // Créons une copie de la tâche concernée tout en modifiant son état
-        const item = { ...state[idx], done: !state[idx].done };
-        // Créons une copie du tableau d'origine
-        const stateCopy = [...state];
-        // Enfin remplaçons la tâche originale par la copie :
-        stateCopy[idx] = item;
-        // Et faisons évoluer le state : l'ancien tableau sera
-        // remplacé par le nouveau, et le rendu sera déclenché à nouveau
-        setState(stateCopy);
+
+        // Créons une copie de la tâche concernée 
+        const item = { ...state[idx] };
+
+        // Appel HTTP en PATCH pour modifier la tâche
+        toggleTaskInApi(id, !item.done).then(() => {
+            // Lorsque le serveur a pris en compte la demande et nous a répond
+            // Nous modifions notre copie de tâche :
+            item.done = !item.done;
+
+            // Créons une copie du tableau d'origine
+            const stateCopy = [...state];
+            // Enfin remplaçons la tâche originale par la copie :
+            stateCopy[idx] = item;
+            // Et faisons évoluer le state : l'ancien tableau sera
+            // remplacé par le nouveau, et le rendu sera déclenché à nouveau
+            setState(stateCopy);
+        });
     }
 
     const addNewTask = (text) => {
         // Créons une nouvelle tâche avec le text tapé dans l'input
         const task = {
-            id: Date.now(),
             text: text,
             done: false
         };
 
-        // Remplaçons le tableau de tâches actuel par une copie
-        // qui contiendra en plus la nouvelle tâche :
-        setState([...state, task]);
+        addTaskToApi(task).then((savedTask) => {
+            // Remplaçons le tableau de tâches actuel par une copie
+            // qui contiendra en plus la nouvelle tâche :
+            setState([...state, savedTask]);
+        })
     }
-    
-    // Pour que nos composants profitent du state et des fonctions
-    // associées, on leur transmet les informations nécessaires
-    // via le biais des props
+
+    // On utilise le hook useEffect, qui permet de créer un comportement
+    // qui aura lieu lors de CHAQUE rendu du composant React
+    // mais en passant un tableau de dépendances vide en deuxième paramètres, on explique à React que ce comportement 
+    // ne devra avoir lieu qu'une seule fois, au chargement du composant
+    useEffect(() => {
+        // Appel HTTP vers Supabase
+        loadTasksFromApi()
+            .then((items) => {
+                // On remplace la valeur actuel de state
+                // par le tableau d'items venant de l'API
+                setState(items);
+            });
+    }, []);
+
     return <>
         <TodoList tasks={state} onTaskToggle={toggle} />
         <TaskForm onTaskAdded={addNewTask} />
