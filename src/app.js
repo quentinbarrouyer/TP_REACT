04065db3e09817c2f41c62775b35ae45ba226000
quentin <b18001457>
2023-03-01 09:39:07 +0100

    todo liste

--- src/app.js
+++ src/app.js
@@ -1,24 +1,12 @@
+// src/app.js
+
 // React va permettre de dessiner notre arbre d'éléments HTML
 import React from "react";
 // ReactDOM va permettre de créer le rendu correspondant dans le DOM HTML
 import ReactDOM from "react-dom";
+// On importe la fonction TodoList
+import TodoList from "./components/TodoList";
 
-/**
- * Créé et retourne un arbre d'éléments React
- * @returns React.DetailedReactHTMLElement
- */
-const Hello = () => {
-    // On créé un élément <div> qui aura les classes "example-class" et "example-class-2", ainsi que l'id "lorem-id"
-    return React.createElement('div', { className: "example-class example-class-2", id: "lorem-id" }, [
-        // Contenant un élement <p>
-        React.createElement('p', {}, [
-            // Contenant lui même un élément <strong> qui contient la string "Hello "
-            React.createElement('strong', {}, "Hello "),
-            // Et un élément TEXT "World"
-            "World"
-        ])
-    ]);
-}
 
-// Imprime l'arbre renvoyé par hello() dans l'élément <main> du DOM HTML
-ReactDOM.render(React.createElement(Hello), document.querySelector('main'));
+// Imprime l'arbre renvoyé par TodoList() dans l'élément <main> du DOM HTML
+ReactDOM.render(<TodoList />, document.querySelector('main'));
---
+++ src/components/TodoList.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+// On créé ici un tableau TODO_ITEMS qui contient deux objets 
+const TODO_ITEMS = [
+    { id: 1, text: "Faire les courses", done: false },
+    { id: 2, text: "Aller chercher les enfants", done: true },
+];
+
+const TodoList = () => {
+    // Créons un état (qui pourra évoluer dynamiquement) à partir du tableau TODO_ITEMS
+    const [state, setState] = useState(TODO_ITEMS);
+
+    const toggle = (id) => {
+        // Récupérons l'index de la tâche concernée
+        const idx = state.findIndex(task => task.id === id);
+        // Créons une copie de la tâche concernée tout en modifiant son état
+        const item = { ...state[idx], done: !state[idx].done };
+        // Créons une copie du tableau d'origine
+        const stateCopy = [...state];
+        // Enfin remplaçons la tâche originale par la copie :
+        stateCopy[idx] = item;
+        // Et faisons évoluer le state : l'ancien tableau sera
+        // remplacé par le nouveau, et le rendu sera déclenché à nouveau
+        setState(stateCopy);
+    }
+
+    // On retourne une list <ul> qui contient un tableau d'éléments React
+    // Chaque objet de TODO_ITEMS sera transformé en un <li> contenant les détails de la tâche
+    // Les éléments React générés par une boucle doivent avoir une "key" unique
+    return <ul>
+        {state.map(item => <li key={item.id}>
+            <label>
+                <input type="checkbox" id="todo-${item.id}" checked={item.done} onChange={() => toggle(item.id)} />
+                {item.text}
+            </label>
+        </li>)}
+    </ul>
+}
+
+export default TodoList;
