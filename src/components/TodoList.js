c9bce7020cf6b122b5c673c7e18926a3c842137d
quentin <b18001457>
2023-03-01 09:52:58 +0100

    Ajout d'une nouvelle tâche

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -24,17 +24,62 @@ const TodoList = () => {
         setState(stateCopy);
     }
 
+    // Créons un état (qui pourra évoluer dynamiquement) qui représentera
+    // la chaîne tapée par l'utilisateur dans l'<input>
+    const [text, setText] = useState('');
+
+
+    // La fonction sera appelée à chaque changement sur l'<input>
+    // Elle recevra les détails de l'événement
+    const updateText = (event) => {
+        // On fait évoluer le state "text" en remplaçant la valeur
+        // acutelle par la valeur mise à jour de l'<input>
+        setText(event.target.value);
+    }
+
+
+
+    // Cette fonction sera appelée lors de la soumission du <form> et recevra les détails de l'événement
+    const handleSubmit = (event) => {
+        // Annulons le comportement par défaut de l'événement
+        // qui serait de recharger la page
+        event.preventDefault();
+
+        // Créons une nouvelle tâche avec le text tapé dans l'input
+        const task = {
+            id: Date.now(),
+            text: text,
+            done: false
+        };
+
+        // Remplaçons le tableau de tâches actuel par une copie
+        // qui contiendra en plus la nouvelle tâche :
+        setState([...state, task]);
+    }
+
     // On retourne une list <ul> qui contient un tableau d'éléments React
     // Chaque objet de TODO_ITEMS sera transformé en un <li> contenant les détails de la tâche
     // Les éléments React générés par une boucle doivent avoir une "key" unique
-    return <ul>
-        {state.map(item => <li key={item.id}>
-            <label>
-                <input type="checkbox" id="todo-${item.id}" checked={item.done} onChange={() => toggle(item.id)} />
-                {item.text}
-            </label>
-        </li>)}
-    </ul>
+    return <>
+        <ul>
+            {state.map(item => <li key={item.id}>
+                <label>
+                    <input type="checkbox" id="todo-${item.id}" checked={item.done} onChange={() => toggle(item.id)} />
+                    {item.text}
+                </label>
+            </li>)}
+        </ul>
+        <form onSubmit={handleSubmit}>
+            <input 
+            type="text" 
+            name="todo-text" 
+            placeholder="Ajouter une tâche" 
+            onChange={updateText} 
+            value={text} 
+            />
+            <button>Ajouter</button>
+        </form>
+    </>
 }
 
 export default TodoList;
